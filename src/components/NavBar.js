d09b025b50b86bb332055d8e0f12e6381b30bedf
Ujjwal Rai <96328364+ujjwal-rai@users.noreply.github.com>
2025-02-26 20:43:12 +0530

    icon update

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -50,12 +50,12 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="https://www.linkedin.com/in/ujjwal-rai-852389227/"><img src={navIcon1} alt="LinkedIn" /></a>
+                <a href="https://github.com/ujjwal-rai"><img src={navIcon2} alt="GitHub" /></a>
                 <a href="#"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
-                <button className="vvd"><span>Let’s Connect</span></button>
+                <button className="vvd"><span>Let's Connect</span></button>
               </HashLink>
             </span>
           </Navbar.Collapse>
